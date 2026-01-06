17f289eba9b92d2765df12972f0380a7b9ea6628
Beyza Onar <beyzaonarr@example.com>
2026-01-07 00:33:00 +0300

    added unit test

--- app/src/test/java/msku/ceng/madlab/readshare/ExampleUnitTest.java
+++ app/src/test/java/msku/ceng/madlab/readshare/ExampleUnitTest.java
@@ -1,17 +1,36 @@
 package msku.ceng.madlab.readshare;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Author: Beyza Onar & Zeynep Demir
+ * Bu test sınıfı, readShare projesindeki veri tutarlılığını doğrular.
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void studentData_isConsistent() {
+        // readShare: Öğrenci profil oluşturma testi [cite: 34]
+        Student student = new Student();
+
+        // Örnek senaryo: Ayşe için veri girişi
+        student.setName("Ayşe");
+
+        // Örnek senaryo: Kitap ihtiyacı girişi [cite: 11, 41]
+        student.setBookNeed("Nutuk");
+
+        // Verilerin doğruluğunu kontrol ediyoruz
+        assertEquals("Ayşe", student.getName());
+        assertEquals("Nutuk", student.getBookNeed());
+    }
+
+    @Test
+    public void donationStatus_isInitialValueWaiting() {
+        // readShare: Yeni bir bağış talebi varsayılan olarak "Waiting" durumundadır
+        Student student = new Student();
+        student.setStatus("Waiting");
+
+        assertEquals("Waiting", student.getStatus());
     }
 }

73a533ff91d71c9d5f0ca934a08947dfca27d5e7
Beyza Onar <beyzaonarr@example.com>
2026-01-07 00:40:16 +0300

    unit test upload

--- app/src/test/java/msku/ceng/madlab/readshare/ExampleUnitTest.java
+++ app/src/test/java/msku/ceng/madlab/readshare/ExampleUnitTest.java
@@ -11,23 +11,23 @@ public class ExampleUnitTest {
 
     @Test
     public void studentData_isConsistent() {
-        // readShare: Öğrenci profil oluşturma testi [cite: 34]
+
         Student student = new Student();
 
-        // Örnek senaryo: Ayşe için veri girişi
+
         student.setName("Ayşe");
 
-        // Örnek senaryo: Kitap ihtiyacı girişi [cite: 11, 41]
+
         student.setBookNeed("Nutuk");
 
-        // Verilerin doğruluğunu kontrol ediyoruz
+
         assertEquals("Ayşe", student.getName());
         assertEquals("Nutuk", student.getBookNeed());
     }
 
     @Test
     public void donationStatus_isInitialValueWaiting() {
-        // readShare: Yeni bir bağış talebi varsayılan olarak "Waiting" durumundadır
+
         Student student = new Student();
         student.setStatus("Waiting");
 
